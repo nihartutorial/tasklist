0a7e1981ccb9ac17f2f123eea2bd38ec0fcaa238
Nihar Donthireddy <nihartutorial@gmail.com>
2021-01-02 19:04:47 -0800

    reading task list data from database

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from db import getTaskList
 app = Flask(__name__)
 
+#tasklist = [["Walk Dog", True], ["Wash Dishes", False], ["Take Out Trash", True]]
+tasklist = getTaskList()
 
 @app.route("/")
 def home():
-    return "Hello"
+    return render_template("tasklist.html", TaskList=tasklist)
 
 
 app.run()
