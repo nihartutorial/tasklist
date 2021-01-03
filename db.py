38ea874c4ab3454bbf1d64cad075db92482e8d8e
Nihar Donthireddy <nihartutorial@gmail.com>
2021-01-03 15:23:06 -0800

    pushed everything

--- db.py
+++ db.py
@@ -9,13 +9,14 @@ def getTaskList():
     cur = conn.cursor()
     cur.execute('SELECT task_name, is_done FROM public."TaskList"')
     tasklist = cur.fetchall()
-    cur.close
-    conn.close
+    cur.close()
+    conn.close()
     return tasklist
 
 def addTask(name, date):
     conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
     cur = conn.cursor()
-    cur.execute('INSERT INTO public."TaskList"(task_name, due_date) values(\'%s\', \'%s\');commit;' % (name, date))
-    cur.close
-    conn.close
+    cur.execute('INSERT INTO public."TaskList"(task_name, due_date) values(\'%s\', \'%s\');' % (name, date))
+    conn.commit()
+    cur.close()
+    conn.close()
