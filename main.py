2369913b3fa386dace583e5ec40923d2954cef72
Nihar Donthireddy <nihartutorial@gmail.com>
2021-01-03 15:03:16 -0800

    Added rows to DB

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
-from db import getTaskList
+from flask import Flask, render_template, request, redirect, url_for
+from db import getTaskList, addTask
 app = Flask(__name__)
 
 #tasklist = [["Walk Dog", True], ["Wash Dishes", False], ["Take Out Trash", True]]
-tasklist = getTaskList()
+
 
 @app.route("/")
 def home():
+    tasklist = getTaskList()
     return render_template("tasklist.html", TaskList=tasklist)
 
 
+@app.route("/add", methods=['POST'])
+def add():
+    taskname = request.form['taskName']
+    duedate = request.form['dueDate']
+    addTask(taskname, duedate)
+    return redirect(url_for('home'))
 app.run()
