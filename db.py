bb3f3f565495f110c0abe56b3891bca70ddbd18a
Nihar Donthireddy <nihartutorial@gmail.com>
2021-01-24 14:29:41 -0800

    Edit and Delete Tasks

--- db.py
+++ db.py
@@ -7,16 +7,28 @@ db_host = "localhost"
 def getTaskList():
     conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
     cur = conn.cursor()
-    cur.execute('SELECT task_name, is_done FROM public."TaskList"')
+    cur.execute('SELECT id, task_name, is_done FROM public."TaskList"')
     tasklist = cur.fetchall()
     cur.close()
     conn.close()
     return tasklist
 
-def addTask(name, date):
+def executeQuery(query):
     conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
     cur = conn.cursor()
-    cur.execute('INSERT INTO public."TaskList"(task_name, due_date) values(\'%s\', \'%s\');' % (name, date))
+    cur.execute(query)
     conn.commit()
     cur.close()
     conn.close()
+
+
+def addTask(name, date):
+    executeQuery('INSERT INTO public."TaskList"(task_name, due_date) values(\'%s\', \'%s\');' % (name, date))
+
+
+def updateTask(name, id):
+    executeQuery('UPDATE public."TaskList" SET task_name=\'%s\' WHERE id=%s;' % (name, id))
+
+
+def deleteTask(id):
+    executeQuery('DELETE FROM public."TaskList" WHERE id=%s;' % (id))
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from db import getTaskList, addTask
+from db import getTaskList, addTask, updateTask, deleteTask
 app = Flask(__name__)
 
 #tasklist = [["Walk Dog", True], ["Wash Dishes", False], ["Take Out Trash", True]]
@@ -17,4 +17,17 @@ def add():
     duedate = request.form['dueDate']
     addTask(taskname, duedate)
     return redirect(url_for('home'))
+
+
+@app.route("/update", methods=['POST'])
+def update():
+    updatedtaskname = request.form['updateTask']
+    id = request.form['id']
+    button = request.form['saveOrDelete']
+    if button == "save":
+        updateTask(updatedtaskname, id)
+    elif button == "X":
+        deleteTask(id)
+    return redirect(url_for('home'))
+
 app.run()
